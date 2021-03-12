1ec13d9418ff25cbae266512dd66b2bab14f1c79
Jason Parr <parr.jason.01@gmail.com>
2021-03-12 12:17:31 -0800

    initial commit w/html, css and some of the JS.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+const score = 0;
+const aWidth;
+const aHeight;
+const timer;
+const iterations;
+
+window.addEventListener("load", setGameAreaBounds);
+
+function setGameAreaBounds() {
+  aWidth = innerWidth;
+  aHeight = innerHeight;
+  aWidth -= 22;
+  aWidth -= 97;
+  document.getElementById("gameArea").style.width = aWidth + "px";
+  document.getElementById("gameArea").style.height = aHeight + "px";
+  document.getElementById("dot").addEventListener("click", detectHit);
+}
+
+function detectHit() {
+  score += 1;
+  document.getElementById("scoreLabel").innerHTML = "Score: " + score;
+
+}
+
+
+
+
+
+
+
+
   // Add your Javascript Here
 })
