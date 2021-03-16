6a0daba774ae8f805a7f7d2cb6bfd1c12923a886
Jason Parr <parr.jason.01@gmail.com>
2021-03-16 15:10:46 -0700

    fixed error in timeout.  current game functions properly.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -4,7 +4,7 @@ let score = 0;
 let aWidth;
 let aHeight;
 let timer;
-let iterations;
+let iterations = 0;
 
 window.addEventListener("load", setGameAreaBounds);
 
@@ -15,6 +15,7 @@ function setGameAreaBounds() {
   aWidth -= 97;
   document.getElementById("gameArea").style.width = aWidth + "px";
   document.getElementById("gameArea").style.height = aHeight + "px";
+  document.getElementById("dot").addEventListener("click", detectHit);
   aWidth -= 74;
   aHeight -= 74;
   moveDot();
@@ -23,10 +24,30 @@ function setGameAreaBounds() {
 function detectHit() {
   score += 1;
   document.getElementById("scoreLabel").innerHTML = "Score: " + score;
-
 }
 
-document.getElementById("dot").addEventListener("click", detectHit);
+function moveDot() {
+let x = Math.floor(Math.random()*aWidth);
+let y = Math.floor(Math.random()*aHeight);
+if (x<10)
+  x = 10;
+if (y<10)
+  y = 10;
+  document.getElementById("dot").style.left = x + "px";
+  document.getElementById("dot").style.top = y + "px";
+  if (iterations < 30)
+    timer = setTimeout(moveDot, 1000);
+  else {
+    document.getElementById("scoreLabel").innerHTML += "     GameOver!";
+    document.getElementById("dot").removeEventListener("click", detectHit);
+    clearTimeout(timer); 
+  }
+  iterations++
+}
+
+
+
+
 
 
 
